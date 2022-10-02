ae00188d976162fd49a0292a6b0e55b1bb0fe2ec
dmytrohaidaienko <dmytrohaidaienko@gmail.com>
2022-10-02 23:40:31 +0300

    Finished work with exercise №6.

--- exercise_6/Program.cs
+++ exercise_6/Program.cs
@@ -15,6 +15,39 @@ namespace exercise_6
     {
         static void Main(string[] args)
         {
+            Int32[] array = new Int32[12];
+
+            FillArray(ref array);
+
+            Console.WriteLine("\n Your array: ");
+            DisplayArray(in array);
+
+            Console.WriteLine("\n New array: ");
+            ReplaceElements(ref array);
+            DisplayArray(in array);
+        }
+
+        static void FillArray(ref Int32[] array)
+        {
+            Random random = new Random();
+
+            for (Int32 i = 0; i < array.Length; i++)
+                array[i] = random.Next(-20, 10);
+        }
+
+        static void DisplayArray(in Int32[] array)
+        {
+            for (Int32 i = 0; i < array.Length; i++)
+                Console.Write($" {array[i]} ");
+        }
+
+        static void ReplaceElements(ref Int32[] array)
+        {
+            for (Int32 i = 0; i < array.Length; i++)
+            {
+                if (array[i] < 0)
+                    array[i] = 0;
+            }
         }
     }
 }
