13b222b40b654d963625bde4fc055ad9d14376b3
dmytrohaidaienko <dmytrohaidaienko@gmail.com>
2022-10-02 18:56:31 +0300

    Added task for exercise №2.

---
+++ exercise_2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise_2
+{
+    /*
+    Задание №2: 
+    Введите с клавиатуры пять целочисленных элементов массива X. Выведите на экран значения корней и квадратов каждого из элементов массива.
+    */
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
