5907cf21b0f2b17838d29b2a71e183c278853797
dmytrohaidaienko <dmytrohaidaienko@gmail.com>
2022-10-02 19:08:48 +0300

    Finished work with exercise №2.

--- exercise_2/Program.cs
+++ exercise_2/Program.cs
@@ -15,6 +15,22 @@ namespace exercise_2
     {
         static void Main(string[] args)
         {
+            Int32[] array = new Int32[5];
+
+            for (Int32 i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine($" Please enter {i + 1} element in array: ");
+                array[i] = Int32.Parse(Console.ReadLine());
+            }
+
+            for (Int32 i = 0; i < array.Length; i++)
+            {
+                Double arraySqrt = Math.Sqrt(array[i]);
+                Console.WriteLine($"Sqrt {i + 1} element: {arraySqrt}");
+
+                Double arrayPow = Math.Pow(array[i], 2);
+                Console.WriteLine($"Pow {i + 1} element: {arrayPow} \n");
+            }
         }
     }
 }
