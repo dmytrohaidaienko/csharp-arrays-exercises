570cf72b4b1b8996b2cd5508ddd83ba52329c5b9
dmytrohaidaienko <dmytrohaidaienko@gmail.com>
2022-10-02 21:39:36 +0300

    Added task for exercise №4.

---
+++ exercise_4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise_4
+{
+
+    /*
+    Задание №5: 
+    Создайте массив А[1..7] с помощью генератора случайных чисел и выведите его на экран. Увеличьте все его элементы в 2 раза. 
+    */
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
