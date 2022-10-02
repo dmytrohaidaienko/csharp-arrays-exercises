11c8950923a6c977f46bdb39f58034d1c7010090
dmytrohaidaienko <dmytrohaidaienko@gmail.com>
2022-10-02 23:30:29 +0300

    Finished work with exercise №5.

--- exercise_5/Program.cs
+++ exercise_5/Program.cs
@@ -15,6 +15,44 @@ namespace exercise_5
     {
         static void Main(string[] args)
         {
+            Int32[] array = new Int32[8];
+
+            FillArray(ref array);
+
+            Console.WriteLine("Your array: ");
+            DisplayArray(in array);
+
+            UInt32 counter = 0;
+
+            CountNegativeElement(in array, ref counter);
+            Console.WriteLine("\n Amount of negative element in array: {0}", counter);
+        }
+
+        static void FillArray(ref Int32[] array)
+        {
+            Random random = new Random();
+
+            for (Int32 i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(-10, 10);
+            }
+        }
+
+        static void DisplayArray(in Int32[] array)
+        {
+            for (Int32 i = 0; i < array.Length; i++)
+            {
+                Console.Write($" {array[i]} ");
+            }
+        }
+
+        static void CountNegativeElement(in Int32[] array, ref UInt32 counter)
+        {
+            for (Int32 i = 0; i < array.Length; i++)
+            {
+                if (array[i] < 0)
+                    counter++;
+            }
         }
     }
 }
