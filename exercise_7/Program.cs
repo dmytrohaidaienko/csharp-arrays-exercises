11976cfb9316520992020108c6d9692921c60060
dmytrohaidaienko <dmytrohaidaienko@gmail.com>
2022-10-02 23:47:55 +0300

    Added task for exercise №7.

---
+++ exercise_7/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise_7
+{
+
+    /*
+    Задание №7:
+    Создайте целочисленный массив А[1..15] с помощью генератора случайных чисел с элементами от –15 до 30 и выведите его на экран. Определите самый большой элемент массива и его индекс.
+    */
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
