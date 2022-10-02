b756f86804b501324e31022de6d4374b5bfd82e5
dmytrohaidaienko <dmytrohaidaienko@gmail.com>
2022-10-02 21:29:43 +0300

    Added task for exercise №3.

---
+++ exercise_3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise_3
+{
+    /*
+    Задание №3: 
+    Создайте массив из пяти фамилий и выведите их на экран столбиком, начиная с последней.
+    */
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
