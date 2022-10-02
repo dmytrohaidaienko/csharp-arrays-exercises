1e543d7c28b2aa967abca64343d663247bb2fbad
dmytrohaidaienko <dmytrohaidaienko@gmail.com>
2022-10-02 23:21:14 +0300

    Added task for exercise №5.

---
+++ exercise_5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise_5
+{
+    /*
+    Задание №5
+    Создайте массив А[1..8] с помощью генератора случайных чисел с элементами от –10 до 10 и выведите его на экран. Подсчитайте количество отрицательных элементов массива. 
+    */
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
