742c57ef23004504462fc5bf68c76aed798066e2
dmytrohaidaienko <dmytrohaidaienko@gmail.com>
2022-10-02 23:33:12 +0300

    Added task for exercise №6.

---
+++ exercise_6/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise_6
+{
+    /*
+    Задание №6:
+    Создайте массив А[1..12] с помощью генератора случайных чисел с элементами от –20 до 10 и выведите его на экран. Замените все отрицательные элементы массива числом 0
+    */
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
