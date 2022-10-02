5e328b5c2125d7cb77f41bef3386cdd53115906a
dmytrohaidaienko <dmytrohaidaienko@gmail.com>
2022-10-02 18:24:14 +0300

    Added task for exercise №1.

---
+++ exercise_1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise_1
+{
+    /*
+    Задание №1: 
+    Введите с клавиатуры в массив пять целочисленных значений. Выведите их в одну строку через запятую. Получите для массива среднее арифметическое.
+    */
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
