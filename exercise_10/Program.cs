642f16ac8a028848dbf724ea489084a696ba9c87
dmytrohaidaienko <dmytrohaidaienko@gmail.com>
2022-10-03 15:13:00 +0300

    Added task for exercise №10.

---
+++ exercise_10/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise_10
+{
+
+    /*
+    Задание №10:
+    Данные о температуре воды на Черноморском побережье за декаду сентября хранятся в массиве. Определить, сколько за это время было дней, пригодных для купания. 
+    */
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
