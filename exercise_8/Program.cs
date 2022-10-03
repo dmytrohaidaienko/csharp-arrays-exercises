b8ac4fd42859d1f39362a4a8c0d9027b273e991e
dmytrohaidaienko <dmytrohaidaienko@gmail.com>
2022-10-03 14:31:20 +0300

    Added task for exercise №8.

---
+++ exercise_8/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise_8
+{
+    /*
+    Задание №8:
+    С 8 до 20 часов температура воздуха измерялась ежечасно. Известно, что в течение этого времени температура понижалась. 
+    Определите, в котором часу была впервые отмечена отрицательная температура.
+    */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
