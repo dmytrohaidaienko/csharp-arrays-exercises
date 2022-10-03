52829146eabcc001e6f617f5d2ab5fd95ae249b6
dmytrohaidaienko <dmytrohaidaienko@gmail.com>
2022-10-03 23:50:03 +0300

    Finished work with exercise №11.

--- exercise_11/Program.cs
+++ exercise_11/Program.cs
@@ -16,6 +16,56 @@ namespace exercise_11
     {
         static void Main(string[] args)
         {
+            Int32[] array = new Int32[10];
+            UInt32 counter = 0;
+            UInt32 temperatureCounter = 0;
+
+            FillArray(ref array);
+
+            Console.WriteLine(" Array of temperature: ");
+            DisplayArray(in array, ref counter);
+
+            CountTemperature(in array, ref temperatureCounter);
+            Console.WriteLine(" How many times the temperature was above the average for this decade: {0}", temperatureCounter);
+        }
+
+        static void FillArray(ref Int32[] array)
+        {
+            Random random = new Random();
+
+            for (Int32 i = 0; i < array.Length; i++)
+                array[i] = random.Next(-15, 5);
+        }
+
+        static void DisplayArray(in Int32[] array, ref UInt32 counter)
+        {
+            if (counter >= array.Length)
+                return;
+
+            Console.Write($" {array[counter]} ");
+            counter++;
+
+            DisplayArray(in array, ref counter);
+        }
+
+        static void CountTemperature(in Int32[] array, ref UInt32 temperatureCounter)
+        {
+            Int32 sum = 0;
+
+            for (Int32 i = 0; i < array.Length; i++)
+            {
+                sum += array[i];
+            }
+
+            Int32 meanArray = sum / array.Length;
+
+            for (Int32 i = 0; i < array.Length; i++)
+            {
+                if (array[i] > meanArray)
+                    temperatureCounter++;
+            }
+
+            Console.WriteLine("\n Mean of array: {0}", meanArray);
         }
     }
 }
