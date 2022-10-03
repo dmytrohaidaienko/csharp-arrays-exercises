cb97cd2e83b89018aae38f9ff23f0b3ce0ecd463
dmytrohaidaienko <dmytrohaidaienko@gmail.com>
2022-10-03 15:26:24 +0300

    Finished work with exercise №10.

--- exercise_10/Program.cs
+++ exercise_10/Program.cs
@@ -16,6 +16,49 @@ namespace exercise_10
     {
         static void Main(string[] args)
         {
+            String[] array = new String[10]
+            {
+                "Available",
+                "Not Avaible",
+                "Available",
+                "Available",
+                "Not Avaible",
+                "Available",
+                "Not Avaible",
+                "Not Avaible",
+                "Available",
+                "Not Avaible"
+            };
+
+            UInt32 firstCounter = 0;
+            UInt32 secondCounter = 0;
+            UInt32 dayCounter = 1;
+
+            Console.WriteLine("Array: ");
+            DisplayArray(in array, ref firstCounter, ref dayCounter);
+
+            CheckDay(in array, ref secondCounter);
+            Console.WriteLine("\nDays with available temperature of water: {0}", secondCounter);
+        }
+
+        static void DisplayArray(in String[] array, ref UInt32 firstCounter, ref UInt32 dayCounter)
+        {
+            if (firstCounter >= array.Length)
+                return;
+
+            Console.WriteLine("Day {0}: {1}", dayCounter, array[firstCounter]);
+            firstCounter++;
+            dayCounter++;
+            DisplayArray(in array, ref firstCounter, ref dayCounter);
+        }
+
+        static void CheckDay(in String[] array, ref UInt32 secondCounter)
+        {
+            for (Int32 i = 0; i < array.Length; i++)
+            {
+                if (array[i] == "Available")
+                    secondCounter++;
+            }
         }
     }
 }
