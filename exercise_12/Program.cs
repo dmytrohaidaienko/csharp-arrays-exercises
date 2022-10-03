2904affc6fbe1517fd3d635aa10cce8ddeb3a5d2
dmytrohaidaienko <dmytrohaidaienko@gmail.com>
2022-10-04 01:22:07 +0300

    Finished work with exercise №12.

--- exercise_12/Program.cs
+++ exercise_12/Program.cs
@@ -16,6 +16,26 @@ namespace exercise_12
     {
         static void Main(string[] args)
         {
+            Int32[] array = new Int32[15] { 12, 1, 6, 2, 48, 3, 18, 11, 33, 54, 62, 91, 15, 6, 4 };
+            UInt32 counter = 0;
+
+            Console.WriteLine(" Your array: ");
+            DisplayArray(in array, ref counter);
+
+            Int32 minElement = array.Min();
+
+            Console.WriteLine("\n The lowest value in the array: {0}", minElement);
+        }
+
+        static void DisplayArray(in Int32[] array, ref UInt32 counter)
+        {
+            if (counter >= array.Length)
+                return;
+
+            Console.Write($" {array[counter]} ");
+            counter++;
+
+            DisplayArray(in array, ref counter);
         }
     }
 }
