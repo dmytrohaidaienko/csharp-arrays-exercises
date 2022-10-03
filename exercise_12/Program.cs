e93361c34175dc5b6a151e0d2328a471da29e56f
dmytrohaidaienko <dmytrohaidaienko@gmail.com>
2022-10-04 01:14:58 +0300

    Added task for exercise №12.

---
+++ exercise_12/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise_12
+{
+
+    /*
+    Задание №12:
+    Создайте массив из 15 целочисленных элементов и определите среди них минимальное значение. 
+    */
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
