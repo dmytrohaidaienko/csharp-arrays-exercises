6e79a4db0a33a3b155ddcdf833d694fe63cb8a98
dmytrohaidaienko <dmytrohaidaienko@gmail.com>
2022-10-03 14:59:48 +0300

    Added task for exercise №9.

---
+++ exercise_9/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise_9
+{
+
+    /*
+    Задание №9:
+    Данные о температуре воздуха за декаду ноября хранятся в массиве. Определить, сколько раз температура опускалась ниже –10 градусов.
+    */
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
