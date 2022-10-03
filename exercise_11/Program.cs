97cde8223d0580dd90a97b79f04503a01845a188
dmytrohaidaienko <dmytrohaidaienko@gmail.com>
2022-10-03 23:35:08 +0300

    Added task for exercise №11.

---
+++ exercise_11/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise_11
+{
+
+    /*
+    Задание №11:
+    Данные о температуре воздуха за декаду декабря хранятся в массиве. Определить, сколько раз температура была выше средней за эту декаду.
+    */
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
