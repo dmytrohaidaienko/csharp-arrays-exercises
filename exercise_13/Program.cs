b5a62771c6ec6ccc7cd900cd04e5ec4ebce121b8
dmytrohaidaienko <dmytrohaidaienko@gmail.com>
2022-10-05 02:40:57 +0300

    Added task for exercise №13.

---
+++ exercise_13/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise_13
+{
+
+    /*
+    Задание №13:
+    Дан линейный массив целых чисел. Проверьте, является ли он упорядоченным по убыванию. 
+    */
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
