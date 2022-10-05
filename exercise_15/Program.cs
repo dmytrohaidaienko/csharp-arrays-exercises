3393702d89b85310d860c27cd416b085c4b1e8ce
dmytrohaidaienko <dmytrohaidaienko@gmail.com>
2022-10-05 03:29:31 +0300

    Added task for exercise №15.

---
+++ exercise_15/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise_15
+{
+
+    /*
+    Задание №15:
+    Найти сумму четных элементов массива целых чисел. Размерность массива – 20. Заполнение массива осуществить случайными числами от 100 до 200. 
+    */
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
