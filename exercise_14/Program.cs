897a789e23a7a15fd43b9bd32477f4da6671d8d2
dmytrohaidaienko <dmytrohaidaienko@gmail.com>
2022-10-05 03:15:33 +0300

    Added task for exercise №14.

---
+++ exercise_14/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise_14
+{
+
+    /*
+    Задание №14:
+    Найти сумму положительных элементов линейного массива целых чисел. Размерность массива – 10. Заполнение массива осуществить с клавиатуры. 
+    */
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
