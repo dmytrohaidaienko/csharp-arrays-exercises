1c2927067690702c88bd504419c9e4a924b2a4bc
dmytrohaidaienko <dmytrohaidaienko@gmail.com>
2022-10-06 23:38:29 +0300

    Added task for exercise №17.

---
+++ exercise_17/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise_17
+{
+
+    /*
+    Задание 17:
+    Найти сумму элементов массива вещественных чисел, имеющих нечетные номера. Размерность массива – 20. Заполнение массива осуществить случайными числами от 100 до 200. 
+    */
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
