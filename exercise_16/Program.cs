42083c388779a42818abf4f9b89504b118c7559d
dmytrohaidaienko <dmytrohaidaienko@gmail.com>
2022-10-06 22:57:31 +0300

    Added task for exercise №16.

---
+++ exercise_16/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise_16
+{
+
+    /*
+    Задание №16:
+    Найти произведение элементов массива целых чисел, которые кратны 7. Размерность массива – 15. Заполнение массива осуществить случайными числами от 10 до 50. 
+    */
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
