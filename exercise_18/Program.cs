cd1b8265b11fa780c4031d74b882f4c3451de292
dmytrohaidaienko <dmytrohaidaienko@gmail.com>
2022-10-09 18:31:15 +0300

    Added task for exercise №18.

---
+++ exercise_18/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise_18
+{
+
+    /*
+    Задание №18:
+    Найти произведение всех элементов массива целых чисел, меньших 0. Размерность массива – 10. Заполнение массива осуществить с клавиатуры. 
+    */
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
