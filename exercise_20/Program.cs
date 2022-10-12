04f0d3d9b4fc0ded86be6b47b2983802a19f4110
dmytrohaidaienko <dmytrohaidaienko@gmail.com>
2022-10-12 16:55:41 +0300

    Added task for exercise №20.

---
+++ exercise_20/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise_20
+{
+
+    /*
+    Задание №20:
+    Найти сумму всех элементов массива целых чисел, которые меньше среднего арифметического элементов массива. 
+    Размерность массива –20. Заполнение массива осуществить случайными числами от 150 до 300. 
+    */
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
