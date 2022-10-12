a4adac8b16aee8f689ca9b16f6785238df3f3510
dmytrohaidaienko <dmytrohaidaienko@gmail.com>
2022-10-12 16:26:39 +0300

    Added task for exercise №19.

---
+++ exercise_19/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise_19
+{
+
+    /*
+    Задание №19:
+    Найти сумму всех элементов массива вещественных чисел, больших заданного числа. Размерность массива – 20. Заполнение массива осуществить случайными числами от 50 до 100. 
+    */
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
