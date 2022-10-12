3e351b4fadea143bc7001109d79f478cec23ec7f
dmytrohaidaienko <dmytrohaidaienko@gmail.com>
2022-10-12 17:10:43 +0300

    Finished work with exercise №20.

--- exercise_20/Program.cs
+++ exercise_20/Program.cs
@@ -17,6 +17,48 @@ namespace exercise_20
     {
         static void Main(string[] args)
         {
+            Int32[] array = new Int32[20];
+            UInt32 counter = 0;
+            Int32 sum = 0;
+
+            FillArray(ref array);
+
+            Console.WriteLine(" Your array: ");
+            DisplayArray(in array, ref counter);
+
+            Double arrayAverage = (Double)array.Average();
+            Console.WriteLine("\n Average of array is {0}", arrayAverage);
+
+            FindSum(in array, in arrayAverage, ref sum);
+            Console.WriteLine(" Sum is {0}", sum);
+        }
+
+        static void FillArray(ref Int32[] array)
+        {
+            Random random = new Random();
+
+            for (Int32 i = 0; i < array.Length; i++)
+                array[i] = random.Next(150, 300);
+        }
+
+        static void DisplayArray(in Int32[] array, ref UInt32 counter)
+        {
+            if (counter >= array.Length)
+                return;
+
+            Console.Write($" {array[counter]} ");
+            counter++;
+
+            DisplayArray(in array, ref counter);
+        }
+
+        static void FindSum(in Int32[] array, in Double arrayAverage, ref Int32 sum)
+        {
+            for (Int32 i = 0; i < array.Length; i++)
+            {
+                if (array[i] < arrayAverage)
+                    sum += array[i];
+            }
         }
     }
 }
