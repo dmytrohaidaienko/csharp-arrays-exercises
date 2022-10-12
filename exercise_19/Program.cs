7fff7292ec6a60c79187b6acd8693ec89575bbad
dmytrohaidaienko <dmytrohaidaienko@gmail.com>
2022-10-12 16:41:13 +0300

    Finished work with exercise №19.

--- exercise_19/Program.cs
+++ exercise_19/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -16,6 +17,48 @@ namespace exercise_19
     {
         static void Main(string[] args)
         {
+            Int32[] array = new Int32[20];
+            UInt32 counter = 0;
+            Int32 sum = 0;
+
+            FillArray(ref array);
+
+            Console.WriteLine("Your array with {0} elements:", array.Length);
+            DisplayArray(in array, ref counter);
+
+            Console.Write("Please, enter value: ");
+            Int32 value = Int32.Parse(Console.ReadLine());
+
+            FindSum(in array, in value, ref sum);
+            Console.WriteLine("The sum of numbers over 20 is {0}", sum);
+        }
+
+        static void FillArray(ref Int32[] array)
+        {
+            Random random = new Random();
+
+            for (Int32 i = 0; i < array.Length; i++)
+                array[i] = random.Next(50, 100);
+        }
+
+        static void DisplayArray(in Int32[] array, ref UInt32 counter)
+        {
+            if (counter >= array.Length)
+                return;
+
+            Console.WriteLine($" {array[counter]} ");
+            counter++;
+
+            DisplayArray(in array, ref counter);
+        }
+
+        static void FindSum(in Int32[] array, in Int32 value, ref Int32 sum)
+        {
+            for (Int32 i = 0; i < array.Length; i++)
+            {
+                if (array[i] > value)
+                    sum += array[i];
+            }
         }
     }
 }
