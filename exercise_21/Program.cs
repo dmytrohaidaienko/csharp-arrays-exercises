d8581955f600547df9818b81352e843026438714
dmytrohaidaienko <dmytrohaidaienko@gmail.com>
2022-10-13 16:21:50 +0300

    Added task for exercise №21.

---
+++ exercise_21/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise_21
+{
+
+    /*
+    Задание №21:
+    Рассортируйте заданный линейный массив по возрастанию.
+    */
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
