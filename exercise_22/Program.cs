535d24ea4c8279df78dabf3a1405429f69e4e0cc
dmytrohaidaienko <dmytrohaidaienko@gmail.com>
2022-10-24 19:25:19 +0300

    Added task for exercise №22.

---
+++ exercise_22/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise_22
+{
+
+    /*
+    Задание №22:
+    Вычислить среднее арифметическое значение тех элементов одномерного массива, которые попадают в интервал от –2 до 10.
+    */
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
