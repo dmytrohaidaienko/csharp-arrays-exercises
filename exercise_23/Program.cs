75abc843dcda9f2d2b7bdf8cac4ed46687f34822
dmytrohaidaienko <dmytrohaidaienko@gmail.com>
2022-10-24 19:44:49 +0300

    Added task for exercise №23.

---
+++ exercise_23/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise_23
+{
+
+    /*
+    Задание №23:
+    Переменной t присвоить значение истина, если в одномерном массиве имеется хотя бы одно отрицательное и четное число.
+    */
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
