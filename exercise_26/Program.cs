6b0ad98d186f5217127db2e1ab40d03b8231592b
dmytrohaidaienko <dmytrohaidaienko@gmail.com>
2022-10-25 16:47:32 +0300

    Finished work with exercise №26.

--- exercise_26/Program.cs
+++ exercise_26/Program.cs
@@ -16,7 +16,35 @@ namespace exercise_26
     {
         static void Main(string[] args)
         {
+            Int32[] array = new Int32[] { 0, 3, 2, 1, 6, 5, 7, 9 };
+            UInt32 displayCounter = 0;
+            UInt32 counter = 0;
 
+            Console.WriteLine(" Your array: ");
+            DisplayArray(in array, ref displayCounter);
+
+            FindAccordance(in array, ref counter);
+            Console.WriteLine("\n Amount of elements which equal to their indexes: {0}", counter);
+        }
+
+        static void DisplayArray(in Int32[] array, ref UInt32 displayCounter)
+        {
+            if (displayCounter >= array.Length)
+                return;
+
+            Console.Write($" {array[displayCounter]} ");
+            displayCounter++;
+
+            DisplayArray(in array, ref displayCounter);
+        }
+
+        static void FindAccordance(in Int32[] array, ref UInt32 counter)
+        {
+            for (Int32 i = 0; i < array.Length; i++)
+            {
+                if (array[i] == i)
+                    counter++;
+            }
         }
     }
 }
