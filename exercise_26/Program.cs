1b2aa6b2a2f04bcebbdf93b5565827e76fbf3724
dmytrohaidaienko <dmytrohaidaienko@gmail.com>
2022-10-25 16:23:20 +0300

    Added task for exercise №26.

---
+++ exercise_26/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise_26
+{
+
+    /*
+    Задание №26:
+    Подсчитайте количество элементов одномерного массива, которые совпадают со своим номером (индексом).
+    */
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
