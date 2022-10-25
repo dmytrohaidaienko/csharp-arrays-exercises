71fb6c726e7a41d5720de659aa7f2b5e4cb9b5fe
dmytrohaidaienko <dmytrohaidaienko@gmail.com>
2022-10-25 20:31:53 +0300

    Added task for exercise №27.

---
+++ exercise_27/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise_27
+{
+
+    /*
+    Задание №27:
+    Создать одномерный массив на 20 элементов. Используя метод сортировки "bubble-sort" отсортировать массив по убыванию.
+    Заполнение массива выполнить случайными числами от -10 до 10.
+    */
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
