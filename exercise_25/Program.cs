bd94540459d284f0f7f8952c4ae8d83788ebf1a2
dmytrohaidaienko <dmytrohaidaienko@gmail.com>
2022-10-25 16:05:42 +0300

    Added task for exercise №25.

---
+++ exercise_25/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise_25
+{
+
+    /*
+    Задание №25:
+    Написать программу благодаря которой будет возможность добавлять и удалять элементы в массиве в не зависимости от расположения элеметов.
+    */
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
