feae6a7b40054f781f3cc10c13f0dd1525294a97
dmytrohaidaienko <dmytrohaidaienko@gmail.com>
2022-10-25 20:58:55 +0300

    Added task for exercise №28.

---
+++ exercise_28/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise_28
+{
+
+    /*
+    Задание №28:
+    Если в одномерном массиве имеются три подряд идущих одинаковых элемента, то переменной r присвоить значение истина.
+    */
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
